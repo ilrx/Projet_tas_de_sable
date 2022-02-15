599cc0242a431b97ad8e77b97f5d4cd67b9bbc21
Aymeric GOUDOUT <aymeric.goudout@ens.uvsq.fr>
2022-02-15 10:38:52 +0100

    Ajout des informations personnelles

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,1 +1,7 @@
-#On essai de PUSH
+#############################################
+# groupe 1
+# Ilerioluwa OLAYODE
+# Aymeric GOUDOUT
+# Sam BARBOSA
+# https://github.com/ilrx/Projet_tas_de_sable
+#############################################
